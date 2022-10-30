e761c0e21366792f55a23b499336d7949628a729
Franco <fnosettoecheverria@hotmail.com>
2022-10-30 20:43:08 -0300

    edlp ecommerce

---
+++ js/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Camiseta Titular 2022", tipo: "camiseta", cantidad: 1, precio: 12000, img: '../img/titular.png'},
+    {id: 2, nombre: "Camiseta Suplente 2022", tipo: "camiseta", cantidad: 1, precio: 11000, img: '../img/suplente.png'},
+    {id: 3, nombre: "Camiseta Alt. 2022", tipo: "camiseta", cantidad: 1, precio: 10000, img: '../img/alternativa.png'},
+    {id: 4, nombre: "Camiseta Arquero 2022", tipo: "camiseta", cantidad: 1, precio: 9000, img: '../img/arquero.png'},
+    {id: 5, nombre: "Campera Ruge", tipo: "campera", cantidad: 1, precio: 9000, img: '../img/campera.png'},
+    {id: 6, nombre: "Camperon Ruge", tipo: "camperon", cantidad: 1, precio: 15000, img: '../img/camperon.png'},
+    {id: 7, nombre: "Short Ruge", tipo: "short", cantidad: 1, precio: 5000, img: '../img/short.png'},
+    {id: 8, nombre: "Medias Ruge", tipo: "medias", cantidad: 1, precio: 900, img: '../img/medias.png'},
+]
